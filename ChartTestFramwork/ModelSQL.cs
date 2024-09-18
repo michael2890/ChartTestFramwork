8163591d2de17ea9110a7d4b0a1e1492dc1a18cb
Toeller <toeller@hhbk.de>
2024-09-18 12:43:42 +0200

    SQL Lauffähig

--- ChartTestFramwork/ModelSQL.cs
+++ ChartTestFramwork/ModelSQL.cs
@@ -13,15 +13,17 @@ namespace ChartTestFramwork
     internal class ModelSQL : IModelLocalData
     {
 
-       private static String myConnectionString = "server=127.0.0.1;uid=EKG;pwd=1234;database=EKG;";
+       private static String myConnectionString = "server=127.0.0.1;uid=EKG;pwd=1234?;database=ekgdatenbank;";
 
        private static MySqlConnection conn = new MySqlConnection(myConnectionString);
 
        private static MySqlCommand mycommand = conn.CreateCommand();
-        IViewEKG IModelLocalData.ViewEKG { set => throw new NotImplementedException(); }
-        IModelECGDevice IModelLocalData.ModelEKGDevice { set => throw new NotImplementedException(); }
-        IControllerEKG IModelLocalData.ControllerEKG { set => throw new NotImplementedException(); }
-
+        IViewEKG IModelLocalData.ViewEKG { set => viewEKG = value; }
+        IModelECGDevice IModelLocalData.ModelEKGDevice { set => modelECGDevice = value; }
+        IControllerEKG IModelLocalData.ControllerEKG { set => controllerEKG = value; }
+        private IViewEKG viewEKG;
+        private IModelECGDevice modelECGDevice;
+        private IControllerEKG controllerEKG;
         List<double> IModelLocalData.getData24h()
         {
             throw new NotImplementedException();
@@ -29,9 +31,9 @@ namespace ChartTestFramwork
 
         void IModelLocalData.saveLiveData(ECGValue aktuellerWert)
         {
-            mycommand.CommandText = "Insert into ekgtabelle  values(NULL,"+
-                aktuellerWert.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss:f:ff") + 
-                "',"+aktuellerWert.Value +" ')";
+            mycommand.CommandText = "Insert into ekgtabelle  values(NULL" +
+                ",'"+ aktuellerWert.Value + "'" +
+                ",'"+aktuellerWert.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss:f:ff") + " ')";
 
             try
             {
@@ -40,6 +42,7 @@ namespace ChartTestFramwork
             }
             catch (Exception ex)
             {
+                MessageBox.Show(mycommand.CommandText);
                 MessageBox.Show(ex.Message);
             }
             finally 
